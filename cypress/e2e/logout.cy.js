5c4ee356f4c053abe5e074ae027daa749813f4b3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-05 22:37:04 +0100

    more fiddling

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -7,10 +7,18 @@ describe("Logout", () => {
     cy.get(".btn-close:visible").click();
     cy.get("button[data-auth='login']").click();
     cy.wait(500);
-    cy.get("input[type='email']:visible").should("exist").type(`jester@noroff.no`);
-    cy.get("input[type='password']:visible").should("exist").type(`password123`);
+    cy.get("input[type='email']:visible")
+      .should("exist")
+      .type(`jester@noroff.no`);
+    cy.get("input[type='password']:visible")
+      .should("exist")
+      .type(`password123`);
     cy.get(".btn-success:visible").click();
     cy.wait(1000);
+    cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
+    cy.then(
+      () => expect(window.localStorage.getItem("profile")).to.not.be.null
+    );
     cy.get("button[data-auth='logout']").click();
     cy.wait(1000);
     cy.url().should("not.include", "profile");
