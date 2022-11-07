711d55a4405a7ec85fbdbc9caa6a30ca95e382e0
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-07 01:14:24 +0100

    no video recording

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      video = false;
       config.env = {
         ...process.env,
         ...config.env,
       };
       return config;
     },
+    video: false,
   },
 });
